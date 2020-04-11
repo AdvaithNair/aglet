ade944ebc6072b82522f6599621429d1ec09e32f
Advaith Nair <advaithnair2@gmail.com>
2020-04-11 02:09:31 -0700

    Updates

---
+++ precache-manifest.e58fbdc1bc32c6300a73aa4cd8e2da75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4917b08ec896d1e7f99cfcb66dbc0923",
+    "url": "/aglet/index.html"
+  },
+  {
+    "revision": "3f5bf17a4134e5f07f53",
+    "url": "/aglet/static/css/main.c10f773a.chunk.css"
+  },
+  {
+    "revision": "1609d0108349c65a1c16",
+    "url": "/aglet/static/js/2.15442c34.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/aglet/static/js/2.15442c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5bf17a4134e5f07f53",
+    "url": "/aglet/static/js/main.9c40d368.chunk.js"
+  },
+  {
+    "revision": "f0266086d2c38f3718fa",
+    "url": "/aglet/static/js/runtime-main.2c99be80.js"
+  },
+  {
+    "revision": "d1e90b8886f2ade309eb6e9ee2876a6e",
+    "url": "/aglet/static/media/AgletLogoOutline.d1e90b88.png"
+  }
+]);
